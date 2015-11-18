ed0ae23906e3be7133cbb76e36a88ccdec68675e
liangqianSheng <ssmtslq@gmail.com>
2015-11-18 11:37:43 -0800

    document cuttable

--- R/cuttable.R
+++ R/cuttable.R
@@ -1,6 +1,15 @@
+#' Make a distribution table for a factor
+#'
+#' @param x factor (numeric)
+#' @param a numeric vector
+#'
+#' @return tbl_df
+#' @export
+#' @examples
+#' cuttable(iris$Sepal.Length,3)
 cuttable<- function(x,a) {
   if(!is.numeric(x)) {
-    stop('I am so sorry, but this function only works for numeric input!\n',
+    stop('I am so sorry, but this function only works for numeric factor input!\n',
          'You have provided an object of class: ', class(x))
   }
   distribution_tbl=cut(x,a)
